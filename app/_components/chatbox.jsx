00de207254dfa3910005fe4466d2e47f716a8b54
Muhammad Hamza <mhamza191703@gmail.com>
2024-08-08 15:51:23 +0500

    Your commit message here

---
+++ app/_components/chatbox.jsx
@@ -0,0 +1,190 @@
+import { useEffect, useRef, useState } from "react";
+import Markdown from "react-markdown";
+import hi from "../../public/hi.gif"
+const Chatbox = () => {
+    const [isChatboxOpen, setIsChatboxOpen] = useState(false);
+    const toggleChatbox = () => setIsChatboxOpen(!isChatboxOpen);
+
+    const [query, setQuery] = useState({
+        role: "user",
+        content: "",
+    });
+
+    const messagesRef = useRef(null);
+    const [input, setInput] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [messages, setMessages] = useState([
+        { role: "system", content: "Hi! How can I assist you?" },
+    ]);
+
+    const scrollToBottom = () => {
+        messagesRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+    const handleQuery = async (e) => {
+        e.preventDefault();
+        if (input === "") {
+            return;
+        }
+        setLoading(true);
+        setQuery((prev) => ({ ...prev, content: input }));
+        setMessages((prev) => [...prev, { role: "user", content: input }]);
+        setInput("");
+        try {
+            const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPEN_API_KEY}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    model: "meta-llama/llama-3.1-8b-instruct:free",
+                    messages: [
+                        { role: "user", content: `Clothing Brand Kingmen Customer Support. ${input}` },
+                    ],
+                    max_tokens: 10,
+                }),
+            });
+
+            if (!res.ok) {
+                setLoading(false);
+                const errorText = await res.text();
+                throw new Error(`Network response was not ok: ${errorText}`);
+            }
+
+            const result = await res.json();
+            const systemResponse = {
+                role: "system",
+                content: result.choices[0].message.content,
+            };
+            setMessages((prev) => [...prev, systemResponse]);
+            setLoading(false);
+        } catch (error) {
+            alert(`Error requesting the model: ${error.message}`);
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <button
+                id="open-chat"
+                className="bg-orange-500 text-white py-2 px-4 rounded-md hover:border-orange-600 transition duration-600 flex items-center"
+                onClick={toggleChatbox}
+            >
+                <img
+                    src="/hi.gif"
+                    alt="Hi Sticker"
+                    className="w-6 h- mr-2"
+                />
+                Customer Service
+            </button>
+
+
+            {isChatboxOpen && (
+                <div
+                    id="chat-container"
+                    className={`fixed bottom-16 right-4 w-96 transition-opacity duration-900 ease-in-out ${isChatboxOpen ? "opacity-100" : "opacity-0"
+                        }`}
+                    style={{ visibility: isChatboxOpen ? "visible" : "hidden" }}
+                >
+                    <div className="bg-white shadow-2xl rounded-lg max-w-lg w-full overflow-hidden">
+                        <div className="p-4 border-b bg-orange-500 text-white rounded-t-lg flex justify-between items-center">
+                            <div className="flex flex-row justify-start align-middle">
+                                <p className="text-lg font-semibold mx-3.5">SilkBot</p>
+                            </div>
+                            <button
+                                id="close-chat"
+                                className="text-orange-300 hover:text-orange-400 focus:outline-none focus:text-orange-400 transition-colors duration-300"
+                                onClick={toggleChatbox}
+                            >
+                                <img
+                                    src="/close.png"
+                                    alt="Chatbot Icon"
+                                    className="w-8 h-8 mr-2"
+                                />
+                            </button>
+                        </div>
+                        <div id="chatbox" className="p-4 h-80 overflow-y-auto">
+                            {messages.map((m, index) => (
+                                <div
+                                    key={index}
+                                    className={`my-1 flex items-start ${m.role === "user" ? "justify-end" : "justify-start"}`}
+                                >
+                                    {m.role === "user" ? (
+                                        <>
+                                            <Markdown
+                                                className={`text-sm p-2 inline-block text-white bg-slate-500 text-end rounded-s-2xl rounded-b-2xl`}
+                                            >
+                                                {m.content}
+                                            </Markdown>
+                                            <img
+                                                src="/profile.png"
+                                                alt="User Icon"
+                                                className="w-8 h-8 ml-2"
+                                            />
+                                        </>
+                                    ) : m.role === "system" && (
+                                        <>
+                                            <img
+                                                src="/service.png"
+                                                alt="Chatbot Icon"
+                                                className="w-8 h-8 mr-2"
+                                            />
+                                            <Markdown
+                                                className={`text-sm p-2 inline-block text-white bg-orange-500 text-left mt-2 mb-2 rounded-e-2xl rounded-b-2xl`}
+                                            >
+                                                {m.content}
+                                            </Markdown>
+                                        </>
+                                    )}
+                                </div>
+                            ))}
+                            <div ref={messagesRef} />
+                        </div>
+
+
+                        <div className="p-4 border-t flex">
+                            {loading && (
+                                <img
+                                    src="/loading.gif"
+                                    alt="Loading"
+                                    className="w-10 h-10 mr-3"
+                                />
+
+                            )}
+                            <form
+                                onSubmit={handleQuery}
+                                className="w-full flex items-center justify-between space-x-3"
+                            >
+                                <input
+                                    id="user-input"
+                                    type="text"
+                                    placeholder="Type a message"
+                                    className="w-full px-3 py-2 border rounded-l-full focus:outline-none focus:ring-2 focus:ring-orange-600"
+                                    value={input}
+                                    onChange={(e) => setInput(e.target.value)}
+                                />
+                                <button
+                                    id="send-button"
+                                    className="bg-orange-500 text-white px-4 py-2 rounded-r-full hover:bg-orange-700 transition-transform transform hover:scale-105 duration-500"
+                                    type="submit"
+                                >
+                                    Send
+                                </button>
+                            </form>
+                        </div>
+
+
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Chatbox;
--- app/page.js
+++ app/page.js
@@ -1,25 +1,23 @@
-"use client"
-// import { useState } from "react";
-// import CustomerSupport from "./_components/CustomerSupport";
+"use client";
 import Link from "next/link";
+import Chatbox from "./_components/Chatbox";  // Corrected import
 
 export default function Home() {
   return (
     <main className="h-screen relative items-center overflow-hidden text-black">
       {/* Nav Section */}
       <nav className="flex items-center justify-center mt-2">
-          <div className="bg-black text-sm space-x-10 text-white rounded-full  md:w-1/2 px-2 py-2 flex items-center justify-center">
-            <a href="/"><button className="bg-black hover:bg-orange-500 rounded-full px-2 md:px-5 py-2">Home</button></a>
-            <h1 className="bg-black hover:text-white hover:bg-orange-500 rounded-full px-2 md:px-5 py-2 font-semibold text-orange-500 text-lg md:text-xl">DEVENGER</h1>
-            <a href="https://head-stater-landing-page.vercel.app" target="_blank"><button className="bg-black hover:bg-orange-500 rounded-full px-2 md:px-5 py-2">Team</button></a>
-          </div>
+        <div className="bg-black text-sm space-x-10 text-white rounded-full  md:w-1/2 px-2 py-2 flex items-center justify-center">
+          <a href="/"><button className="bg-black hover:bg-orange-500 rounded-full px-2 md:px-5 py-2">Home</button></a>
+          <h1 className="bg-black hover:text-white hover:bg-orange-500 rounded-full px-2 md:px-5 py-2 font-semibold text-orange-500 text-lg md:text-xl">DEVENGER</h1>
+          <a href="https://head-stater-landing-page.vercel.app" target="_blank"><button className="bg-black hover:bg-orange-500 rounded-full px-2 md:px-5 py-2">Team</button></a>
+        </div>
       </nav>
       {/* Body Section */}
       <section className="h-full flex flex-col  mt-2 items-center ">
         <div className="text-center h-1/4 text-3xl space-y-1 font-bold">
-            {/* <button className="text-sm px-3 py-1 rounded-xl font-normal border text-black border-orange-500">Customer Support</button> */}
-            <h1>We are <span className="text-orange-500">KINGSMAN</span></h1>
-            <h1>Clothing Brand</h1>
+          <h1>We are <span className="text-orange-500">KINGSMAN</span></h1>
+          <h1>Clothing Brand</h1>
         </div>
         {/* main section */}
         <div className="flex flex-col md:flex-row space-y-8 md:space-y-0  text-center items-center h-2/3 justify-between md:justify-center md:px-8 text-md font-medium">
@@ -30,13 +28,16 @@ export default function Home() {
           {/* Image Section */}
           <div className="w-full h-full md:w-1/2 overflow-hidden flex flex-col items-center text-center relative">
             <div className="absolute top-36 md:top-48 z-10 w-full h-full rounded-full bg-orange-300"></div>
-            <img src="/istockphoto-907865186-612x612-removebg-preview.png" alt="DEVENGER" className="relative md:absolute md:bottom-0 lg:relative drop-shadow-lg object-fill z-20 w-full"/>
-            <div className="absolute bottom-10 md:bottom-28 z-30  space-x-2  rounded-full px-3 py-2 border border-white">
-              <Link href="/customersupport"><button  className="text-white bg-orange-600 text-sm rounded-2xl px-3 py-2">Customer Support<i className="ms-2 bi bi-arrow-up-right"></i></button></Link>
+            <img src="/istockphoto-907865186-612x612-removebg-preview.png" alt="DEVENGER" className="relative md:absolute md:bottom-0 lg:relative drop-shadow-lg object-fill z-20 w-full" />
+            <div className="absolute bottom-10 md:bottom-28 z-30 space-x-2 rounded-full px-3 py-2 border border-white">
+              {/* <Link href="/customersupport"><button className="text-white bg-orange-600 text-sm rounded-2xl px-3 py-2">Customer Support<i className="ms-2 bi bi-arrow-up-right"></i></button></Link> */}
+              <div class="fixed bottom-0 right-0 mb-4 mr-4">
+                <Chatbox />
+              </div>
               <a href="https://github.com/Subhan-hub1917/customer_support" target="_blank"><button className="text-white bg-transparent text-sm rounded-2xl px-3 py-2">Github</button></a>
             </div>
           </div>
-          <div className="hidden md:block text-end   w-1/4">
+          <div className="hidden md:block text-end w-1/4">
             <p className=" text-orange-500 text-lg space-x-1 ">
               <i className="bi bi-star-fill"></i>
               <i className="bi bi-star-fill"></i>
@@ -50,5 +51,5 @@ export default function Home() {
         </div>
       </section>
     </main>
-    );
+  );
 }
